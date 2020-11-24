dc16bf2e5872f28c0d645ee4ac4cc7df6d919764
Dimonil <74609554+Dimonil@users.noreply.github.com>
2020-11-24 20:59:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-def delim(p):
-    flag = True
-    a = str(p)
-    for i in a:
-        if i!='0':
-            if p % int(i) != 0:
-                flag = False
-                break
-        else:
-            flag = False
-            break
-    return flag
+array = [1,3,4,2,9]
 
-def DelimInRange(a,b):
-    array = []
-    for i in range(a,b+1):
-        if delim(i):
-            array+=[i]
-    return array
-print(DelimInRange(1,22))
+
+def differ(a, b):
+    if a >= b:
+        a = a - b
+    else:
+        a = b - a
+    return a
+
+
+def evaluate(arr):
+    even = 1
+    odd = 0
+    for i in arr:
+
+        if i % 2 == 0:
+            even *= i
+        else:
+            odd += i
+
+    return differ(even, odd)
+
+
+x = evaluate(array)
+
+
+print(x)
