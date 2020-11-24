c354382ca092b91a0dfeaf6855203588f451724f
Dimonil <74609554+Dimonil@users.noreply.github.com>
2020-11-24 20:25:09 +0200

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,27 @@
+word = input()
+array = []
+bool_array = [True for i in range(len(word))]
+diff = 96
+
+for i in range(len(word)-1, -1, -1):
+    if bool_array[i] == False:
+        continue
+    if word[i] == '#':
+        array += [word[i-2]+word[i-1]]
+        bool_array[i] = False
+        bool_array[i-1] = False
+        bool_array[i-2] = False
+    else:
+        array += [word[i]]
+
+
+for i in range(len(array)):
+    array[i] = chr(int(array[i])+diff)
+array = array[::-1]
+new_word = ''
+for i in array:
+    new_word += i
+
+print(new_word)
+
+
