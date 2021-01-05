aee7b82ffeeb20e2a7454199afb86380e86feb4d
Dimonil <74609554+Dimonil@users.noreply.github.com>
2021-01-05 21:53:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import pyautogui
 import time
 import cv2
 import numpy
-import keyboard
+
 
 from PIL import ImageGrab
 import keyboard
 # Press Shift+F10 to execute it or replace it with your code.ы
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-tmp = "template.png"
-Hotkey = 'enter'
 bool_repeat = False
 size_screen = pyautogui.size()
 pyautogui.FAILSAFE = True
+dif_mean = 0
 #x 700 , y 450 # 200x25
+if size_screen == (1600, 900):
+    x0, y0 = (700, 450), (785, 470)
+    x1, y1 = (850, 450), (900, 470)
+elif size_screen == (1920, 1080):
+    x0, y0 = (840, 540), (920, 565)
+    x1, y1 = (1015, 540), (1080, 565)
+else:
+    x0, y0 = (840, 540), (920, 565)
+    x1, y1 = (1015, 540), (1080, 565)
+
+
+def no_catch(x, y):
+    if abs(x - y) > 20:
+        return False
+    else:
+        return True
 
 
 def timer(f):
@@ -28,38 +43,49 @@ def timer(f):
     return tmp
 
 
-def make_screen():
-    base_screen = ImageGrab.grab()
-    base_screen.save("screen.png")
+
 
 
 def catch():
-    #print('начал играть')
-    if size_screen==(1600,900):
-        x0, y0 = (700, 450), (785, 470)
-    elif size_screen == (1920,1080):
-        x0, y0 = (840, 540), (940, 565)
+    clean_screen = ImageGrab.grab(bbox=(*x0, *y0))
+    right_screen = ImageGrab.grab(bbox=(*x1, *y1))
 
-    clean_screen = ImageGrab.grab(bbox = (*x0, *y0))
-    clean_screen.save("Catch.png")
 
-    array = [numpy.mean(clean_screen)]
+    clean_screen.save("left_side.png")
+    right_screen.save("right_side.png")
+
+    array_right = [numpy.mean(right_screen)]
+    if no_catch(dif_mean, array_right[-1]):
+        return
+    #array = [numpy.mean(clean_screen)]
 
     for i in range(1000):
         time.sleep(0.05)
-        dif_screen = ImageGrab.grab(bbox=(*x0, *y0))
-        mean = numpy.mean(dif_screen)
-        diff = abs(array[0] - mean)
+        #l_screen = ImageGrab.grab(bbox=(*x0, *y0))
+        r_screen = ImageGrab.grab(bbox=(*x1, *y1))
+        #mean_l = numpy.mean(l_screen)
+        mean_r = numpy.mean(r_screen)
+        #diff_l = abs(array[0] - mean_l)
+        diff_r = abs(array_right[0] - mean_r)
 
-        if diff >= 2:
+        if diff_r <= 1:
+            pyautogui.mouseDown(button='left')
+
+        elif diff_r >= 20:
+            pyautogui.mouseUp(button='left')
+            break
+        else:
+            pyautogui.mouseUp(button='left')
+
+        '''if diff_l >= 2:
             pyautogui.mouseDown(button= 'left')
         else:
             pyautogui.mouseUp(button= 'left')
 
-        if diff >= 20:
+        if diff_l >= 20:
             pyautogui.mouseUp(button='left')
             #print('закончил играть diff=', diff)
-            break
+            break'''
 
 
 def throw_a_hook(x, y):
@@ -76,10 +102,10 @@ def wait():
 
 
 def click_on_hook(x, y):
+
     r = 15
     array = [0]
     for i in range(1000):
-
         clean_screen = ImageGrab.grab(bbox=(x-r, y - r, x + r, y + r))
         #clean_screen.save('clean_screen.png')
 
@@ -88,28 +114,31 @@ def click_on_hook(x, y):
         array.append(mean)
         time.sleep(0.1)
 
-        if diff >= 4:
+        if diff >= 10:
             #print('нажал на крючок', diff)
+            dif_mean = numpy.mean(ImageGrab.grab(bbox=(*x1, *y1)))
             pyautogui.click()
             break
 
 
 if __name__ == '__main__':
     count = 1
-
     coord0 = wait()
+    
     coord1 = wait()
+
     wait()
+
     time.sleep(0.1)
 
     while True:
-        
+
         print(count)
         bool_repeat = False
         throw_a_hook(*coord0)
         time.sleep(1.5)
         click_on_hook(*coord1)
-        time.sleep(0.2)
+        time.sleep(0.1)
         catch()
         time.sleep(2)
         count+=1
